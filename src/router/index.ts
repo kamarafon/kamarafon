53d390c61d59b87d209182d8bfb4c8b4dc8f5dd2
Alexander Degtarov <aleksandr.degtyarev@nexign.com>
2023-09-28 11:42:22 +0300

    add favicon and headers

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
+import type {RouteLocationNormalized} from 'vue-router'
 import Main from '@/views/Main.vue'
 import KamarafonMain from '@/views/kamarafon/KamarafonMain.vue'
 import KamarafonGame from '@/views/kamarafon/KamarafonGame.vue'
@@ -7,6 +8,11 @@ import KamatcherMain from '@/views/kamatcher/KamatcherMain.vue'
 import KamatcherPlayerChoice from '@/views/kamatcher/KamatcherPlayerChoice.vue'
 import KamatcherResults from '@/views/kamatcher/KamatcherResults.vue'
 
+const pageTitleUpdate = (route: RouteLocationNormalized) => {
+  const pageTitle = route.meta.title
+  if (pageTitle) document.title = `${pageTitle}`
+}
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -14,12 +20,20 @@ const router = createRouter({
       path: '/',
       name: 'main',
       component: Main,
+      meta: {
+        title: 'Игры для пары'
+      },
+      beforeEnter: pageTitleUpdate,
     },
 
     {
       path: '/kamarafon',
       name: 'kamarafon-main',
       component: KamarafonMain,
+      meta: {
+        title: 'Kamarafon'
+      },
+      beforeEnter: pageTitleUpdate,
     },
     {
       path: '/kamarafon/:level',
@@ -31,6 +45,10 @@ const router = createRouter({
       path: '/kamatcher',
       name: 'kamatcher',
       component: RouterContainer,
+      meta: {
+        title: 'Kamatcher'
+      },
+      beforeEnter: pageTitleUpdate,
       children: [
         {
           path: '',
@@ -56,7 +74,7 @@ const router = createRouter({
         },
       ]
     },
-  ]
+  ],
 })
 
 export default router
