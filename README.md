c7ffd1bb97c322d5c7a99b3dff86beef1acb64c6
Alexander Degtarov <aleksandr.degtyarev@nexign.com>
2023-09-28 09:49:06 +0300

    add kamatcher description

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 * text-wrap balance
 * p:first-letter { 6.6 3 }
 * height 100svh  /// (svh - small, lvh - large, dvh - dynamic) 100dvh
+* object-view-box // show part of image
+* @media (400px <= width)
+* mix-blend-mode
+* css filter
+* backdrop-filter: blur(10px)
+* width: min(max)-content
+* accent-color // design for all controls
+* touch-action
+* line-clamp // троеточие по высоте
--- src/views/kamatcher/KamatcherMain.vue
+++ src/views/kamatcher/KamatcherMain.vue
@@ -3,7 +3,8 @@ import WelcomeGame from '@/components/WelcomeGame.vue'
 import router from '@/router'
 import {useKamatcherStore} from '@/stores/kamatcher'
 import {Gender} from '@/common/gender'
-import {genderClothesCount} from '@/common/clothes'
+import {genderClothesCount, KamatcherClothesByGender} from '@/common/clothes'
+import * as _ from 'lodash'
 
 const store = useKamatcherStore()
 const cardsNumber = [2, 3, 4]
@@ -15,6 +16,20 @@ const selectCard = (n: number) => {
     query: {w: genderClothesCount(Gender.Woman), m: genderClothesCount(Gender.Man)}
   })
 }
+
+const herClothes = _.values(KamatcherClothesByGender[Gender.Woman])
+const hisClothes = _.values(KamatcherClothesByGender[Gender.Man])
+
+const clothesShouldBe = [
+  {
+    title: 'Она одевает',
+    clothes: herClothes,
+  },
+  {
+    title:  'Он одевает',
+    clothes: hisClothes,
+  }
+]
 </script>
 
 <template>
@@ -22,12 +37,28 @@ const selectCard = (n: number) => {
     <welcome-game>
       <template v-slot:title>kamatcher</template>
       <template v-slot:description>
+        <h3>Подготовка к игре</h3>
+        <p>
+          В этой игре, у каждого из партнёров одето по 5 элементов одежды
+        </p>
+        <div class="clothes-description">
+          <div class="clothes-name" v-for="clothes in clothesShouldBe">
+            <p>{{ clothes.title }}</p>
+            <ul>
+              <li v-for="c in clothes.clothes">{{ c[1] || c[0] }}</li>
+            </ul>
+          </div>
+        </div>
+        <p>Элементы одежды не обязательно должны быть такими же, главное что бы их было 5, на каждом партнёре</p>
+        <h3>Правила игры</h3>
         <ul>
-          <li>трусы</li>
-          <li>галстук (бабочка) / лифчик</li>
-          <li>носки / чулки</li>
-          <li>штаны (шёрты) / юбка</li>
-          <li>рубаха (футболка) / кофта</li>
+          <li>Сначала вы выбираете количество карт на один круг игры (2, 3, 4)</li>
+          <li>Когда игра начинается один из партнёров отворачивается, а другой выбирает позу в которой хотел бы заняться сексом</li>
+          <li>Затем выбор делает другой партнёр</li>
+          <li>Если вы выбирете одну и ту же позу, то вам можно заняться сексом в этой позе</li>
+          <li>Если на вас есть какая-то одежда, то вы занимаетес сексом в одежде (т.е. просто имитеруете половой акт)</li>
+          <li>После каждого хода вы можете потерять одежду, выбора происходит случайно</li>
+          <li>Игра продолжается пока вы не потеряете всю одежду, и полностью голыми не найдёте позу которая понравиться вам обоим</li>
         </ul>
       </template>
     </welcome-game>
@@ -55,4 +86,10 @@ const selectCard = (n: number) => {
 
 .welcome-game
   height 80%
+
+.clothes-description
+  display flex
+
+  .clothes-name
+    margin-right 2em
 </style>
